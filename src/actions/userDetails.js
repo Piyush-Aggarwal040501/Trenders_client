f6bf5940c720bc5565adc8066db88280e63f7cda
Piyush Aggarwal <aggarwalpiyush85@gmail.com>
2024-01-27 12:10:03 +0530

    first commit

---
+++ src/actions/userDetails.js
@@ -0,0 +1,40 @@
+import { server_url } from "../information/backend_url";
+export const userDetailsAction = ()=>{
+    return async (dispatch)=>{
+        try{
+
+            dispatch({
+                type:'userDetailsRequest'
+            })
+
+            const res = await fetch(`${server_url}/api/userDetails`,{
+                method:"GET",
+                headers:{
+                    Accept:"application/json",
+                    "content-Type":"application/json"
+                },
+                Credential:'include'
+            });
+            const {data} = await res.json();
+            if(res.status==200){
+                dispatch({
+                    type:'userDetailsSuccess',
+                    payload:data
+                })
+            }else if(res.status==401){
+                dispatch({
+                    type:'userDetailsNotRegestered'
+                })
+            }else{
+                dispatch({
+                    type:'userDetailsFail',
+                })
+            }
+        }catch(err){
+            dispatch({
+                type:'userDetailsFail',
+            })
+        }
+    }
+}
+
---
+++ src/information/backend_url.js
@@ -0,0 +1,1 @@
+export const server_url = "https://trenders-server.onrender.com";
